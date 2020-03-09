dabc49ab7e2bedc817718299e6f1bd32987c403a
kadirumac <kadirumac49@gmail.com>
2020-03-09 10:38:23 +0100

    basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={`${process.env.PUBLIC_URL}/`}>
       <Route path="/" exact component={Join} />
       <Route path="/chat" component={Chat} />
     </Router>
