7ff637c4cb2bd653409b1e372cdc4f65a1dae9d6
kadirumac <kadirumac49@gmail.com>
2020-03-09 10:28:13 +0100

    Updates

---
+++ precache-manifest.d54e403737d24747533f08656ab8ad4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee75115be9fb16ebc8130a94e3d316b2",
+    "url": "/los-chat/index.html"
+  },
+  {
+    "revision": "71f7e1c21873cc279b85",
+    "url": "/los-chat/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "d25f58751d0fd911b7aa",
+    "url": "/los-chat/static/js/2.256c748c.chunk.js"
+  },
+  {
+    "revision": "71f7e1c21873cc279b85",
+    "url": "/los-chat/static/js/main.6936d2a1.chunk.js"
+  },
+  {
+    "revision": "cf5ed973db2874d3aaca",
+    "url": "/los-chat/static/js/runtime~main.cd4e6a1d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app-with-socket/precache-manifest.41a31b5dabceffdfec69e9b194645f3e.js"
+  "/los-chat/precache-manifest.d54e403737d24747533f08656ab8ad4e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-with-socket/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/los-chat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
