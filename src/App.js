a3f9511d0c7326b8a07999f015c5acb382875157
kadirumac <kadirumac49@gmail.com>
2020-03-09 10:47:37 +0100

    log

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Join from './components/Join/Join';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
+  console.log('connected');
+  
   return (
     <Router basename={`${process.env.PUBLIC_URL}/`}>
       <Route path="/" exact component={Join} />
